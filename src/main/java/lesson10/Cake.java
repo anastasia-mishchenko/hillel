f757f24ff2cb28d8e790c2b1fc93350923e96bae
anastasia-mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2021-07-22 22:58:35 +0300

    add HW static final

---
+++ src/main/java/lesson10/Cake.java
@@ -0,0 +1,7 @@
+package lesson10;
+
+public class Cake extends Sweets{
+    public Cake(int weight) {
+        super(weight);
+    }
+}
--- src/main/java/lesson10/Main.java
+++ src/main/java/lesson10/Main.java
@@ -1,11 +1,18 @@
 package lesson10;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Candy Candy1 = new Candy( 1);
-        Sweets Cake = new Sweets( 2 );
+        Sweets Candy = new Candy( 2);
+        Sweets Cake = new Sweets( 3 );
         Sweets IceCream = new Sweets( 1);
+        Sweets frf = new Sweets(5);
 
-        System.out.println(Cake.getWeight() + Candy1.getWeight() + IceCream.getWeight());
+        System.out.println(Cake.getWeight() + Candy.getWeight() + IceCream.getWeight());
+
+        Sweets [] array = new Sweets[Sweets.count] ;
+
+        System.out.println(Arrays.toString(array));
     }
 }
